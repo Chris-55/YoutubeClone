cdf94255e022cb31b2b2994abb94cc829950f106
SalvaGames <dmitrijus.doulaveris@gmail.com>
2023-02-21 17:03:48 +0200

    search bar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { logo } from '../utils/constants';
+import { SearchBar } from './index'
 
 const Navbar = () => (
     <Stack direction="row" alignItems="center"
@@ -14,6 +15,7 @@ const Navbar = () => (
         <Link to="/" style={{display: 'flex', allignItems: 'center'}}>
             <img src={logo} alt='logo' height={45}/>
         </Link>
+        <SearchBar />
     </Stack>
 )
 
