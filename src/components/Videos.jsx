e160c9c553c442ffaf9116d61e65c70850da9d94
SalvaGames <dmitrijus.doulaveris@gmail.com>
2023-02-23 18:52:13 +0200

    added videocard

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,20 +1,21 @@
-import { Stacl, Box} from '@mui/material';
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = ({ videos}) => {
-    return (
-        <Stack direction="row"
-        flexWrap="wrap"
-        justifyContent="start"
-        gap={2}
-        >
-          {videos.map((item, indx) => (
-            <Box key={idx}>
-                {item.id.videoId && <VideoCard video={item} />}
-                {item.id.videoId && <VideoCard video={item} />}
-            </Box>
-          ))}  
-        </Stack>
-    )
+import { ChannelCard, Loader, VideoCard } from "./index";
+
+const Videos = ({ videos, direction }) => {
+  if (!videos || !videos.length) return <Loader />;
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id && item.id.videoId && <VideoCard video={item} /> }
+          {item.id && item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 }
 
-export default Videos
+export default Videos;
